b322a455160cf9b0d4167879953080efcfea0f5b
michaelfuchaka <fuchakamichael06@gmail.com>
2025-08-07 23:36:01 +0300

    Complete print_fibonacci function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+       if length == 0:
+         print([])
+         return
+       elif length == 1:
+         print([0])
+         return
+
+       fib = [0, 1]
+       while len(fib) < length:
+        fib.append(fib[-1] + fib[-2])
+       print(fib[:length]) 
+       pass
